d8bf518b3347fbf644c89efeaef57c24e880f85c
jeongwoo 조정우 <choeun1999@gmail.com>
2024-03-28 15:26:32 +0900

    조정우 : 엔진 업데이트

--- DirectX2DPortfolio/EngineCore/Level.cpp
+++ DirectX2DPortfolio/EngineCore/Level.cpp
@@ -1,9 +1,14 @@
 #include "PreCompile.h"
 #include "Level.h"
 #include "GameMode.h"
+#include "Renderer.h"
+#include "Camera.h"
+
+bool ULevel::IsActorConstructer = true;
 
 ULevel::ULevel()
 {
+	MainCamera = std::make_shared<UCamera>();
 }
 
 ULevel::~ULevel()
@@ -24,6 +29,19 @@ void ULevel::Tick(float _DeltaTime)
 	}
 }
 
+void ULevel::Render(float _DeltaTime)
+{
+	for (std::pair<const int, std::list<std::shared_ptr<URenderer>>>& RenderGroup : Renderers)
+	{
+		std::list<std::shared_ptr<URenderer>>& GroupRenderers = RenderGroup.second;
+
+		for (std::shared_ptr<URenderer> Renderer : GroupRenderers)
+		{
+			Renderer->Render(_DeltaTime);
+		}
+	}
+}
+
 void ULevel::PushActor(std::shared_ptr<AActor> _Actor)
 {
 	if (nullptr == _Actor)
@@ -32,5 +50,15 @@ void ULevel::PushActor(std::shared_ptr<AActor> _Actor)
 		return;
 	}
 
+	_Actor->SetWorld(this);
+	_Actor->BeginPlay();
+
 	Actors[_Actor->GetOrder()].push_back(_Actor);
+}
+
+
+
+void ULevel::PushRenderer(std::shared_ptr<URenderer> _Renderer)
+{
+	Renderers[_Renderer->GetOrder()].push_front(_Renderer);
 }
