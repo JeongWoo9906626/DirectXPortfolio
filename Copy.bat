62c407750ab22ca70158c1f2b5727bca6c753a12
jeongwoo 조정우 <choeun1999@gmail.com>
2024-04-03 18:02:17 +0900

    조정우 : 엔진 업데이트

---
+++ Copy.bat
@@ -0,0 +1,11 @@
+@echo off 
+xcopy D:\GM\DX\DirectX\EngineBase\*.h D:\DirectXPortfolio\DirectX2DPortfolio\EngineBase\ /e /h /k /y
+xcopy D:\GM\DX\DirectX\EngineCore\*.h D:\DirectXPortfolio\DirectX2DPortfolio\EngineCore\ /e /h /k /y
+xcopy D:\GM\DX\DirectX\EnginePlatform\*.h D:\DirectXPortfolio\DirectX2DPortfolio\EnginePlatform\ /e /h /k /y
+
+xcopy D:\GM\DX\DirectX\EngineBase\*.cpp D:\DirectXPortfolio\DirectX2DPortfolio\EngineBase\ /e /h /k /y
+xcopy D:\GM\DX\DirectX\EngineCore\*.cpp D:\DirectXPortfolio\DirectX2DPortfolio\EngineCore\ /e /h /k /y
+xcopy D:\GM\DX\DirectX\EnginePlatform\*.cpp D:\DirectXPortfolio\DirectX2DPortfolio\EnginePlatform\ /e /h /k /y
+
+xcopy D:\GM\DX\DirectX\EngineShader\*.hlsli D:\DirectXPortfolio\DirectX2DPortfolio\EngineShader\ /e /h /k /y
+xcopy D:\GM\DX\DirectX\EngineShader\*.fx D:\DirectXPortfolio\DirectX2DPortfolio\EngineShader\ /e /h /k /y
--- DirectX2DPortfolio/EngineCore/Renderer.cpp
+++ DirectX2DPortfolio/EngineCore/Renderer.cpp
@@ -1,12 +1,15 @@
 #include "PreCompile.h"
 #include "Renderer.h"
 #include "EngineInputLayOut.h"
+#include "EngineShaderResources.h"
+#include "Camera.h"
 
-URenderer::URenderer() 
+URenderer::URenderer()
 {
+	Resources = std::make_shared<UEngineShaderResources>();
 }
 
-URenderer::~URenderer() 
+URenderer::~URenderer()
 {
 }
 
@@ -14,7 +17,7 @@ void URenderer::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// 해줘야 한다/
+	// 해줘야 한다
 	GetWorld()->PushRenderer(shared_from_this());
 }
 
@@ -32,7 +35,7 @@ void URenderer::Render(float _DeltaTime)
 	// 교육용으로 랜더링파이프라인의 순서에 따라 세팅해주려는 것뿐이지
 	// 꼭 아래의 순서대로 세팅을 해야만 랜더링이 되는게 아니에요.
 	// Mesh->Setting()
-	
+
 	// InputAssembler1
 	Mesh->InputAssembler1Setting();
 	LayOut->Setting();
@@ -49,11 +52,12 @@ void URenderer::Render(float _DeltaTime)
 	// PixelShader
 	Material->PixelShaderSetting();
 
-	// OM
+	Resources->SettingAllShaderResources();
+
 
 	// Draw
 	Mesh->IndexedDraw();
-}	
+}
 
 
 void URenderer::SetMesh(std::string_view _Name)
@@ -86,4 +90,39 @@ void URenderer::SetMaterial(std::string_view _Name)
 	{
 		LayOut = UEngineInputLayOut::Create(Mesh->VertexBuffer, Material->GetVertexShader());
 	}
+
+	ResCopy(Material->GetVertexShader().get());
+	ResCopy(Material->GetPixelShader().get());
+
+	if (true == Resources->IsConstantBuffer("FTransform"))
+	{
+		Resources->SettingConstantBuffer("FTransform", Transform);
+	}
+
+}
+
+void URenderer::ResCopy(UEngineShader* _Shader)
+{
+
+	std::map<EShaderType, std::map<std::string, UEngineConstantBufferSetter>>& RendererConstantBuffers
+		= Resources->ConstantBuffers;
+
+	std::shared_ptr<UEngineShaderResources> ShaderResources = _Shader->Resources;
+
+	std::map<EShaderType, std::map<std::string, UEngineConstantBufferSetter>>& ShaderConstantBuffers
+		= ShaderResources->ConstantBuffers;
+
+	for (std::pair<const EShaderType, std::map<std::string, UEngineConstantBufferSetter>> Setters : ShaderConstantBuffers)
+	{
+		for (std::pair<const std::string, UEngineConstantBufferSetter> ConstantBufferSetter : Setters.second)
+		{
+			RendererConstantBuffers[Setters.first][ConstantBufferSetter.first] = ConstantBufferSetter.second;
+		}
+	}
+}
+
+
+void URenderer::RenderingTransformUpdate(std::shared_ptr<UCamera> _Camera)
+{
+	Transform.CalculateViewAndProjection(_Camera->GetView(), _Camera->GetProjection());
 }
