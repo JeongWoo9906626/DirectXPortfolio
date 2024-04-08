5ea5c2da7054808d8bea191113f6ec94602ab1c4
jeongwoo 조정우 <choeun1999@gmail.com>
2024-04-08 12:24:24 +0900

    조정우 : 엔진 업데이트

--- DirectX2DPortfolio/Contents/TitleLogo.cpp
+++ DirectX2DPortfolio/Contents/TitleLogo.cpp
@@ -13,36 +13,13 @@ ATitleLogo::~ATitleLogo()
 {
 }
 
-void ATitleLogo::RendererOn()
-{
-	Renderer->SetSprite("CuttingTest.png", 11);
-	//Renderer->SetActive(true);
-	DelayCallBack(0.2f, std::bind(&ATitleLogo::RendererOff, this));
-
-	// ++Index;
-}
-
-void ATitleLogo::RendererOff()
-{
-	Renderer->SetSprite("CuttingTest.png", 11);
-	//Renderer->SetActive(false);
-	DelayCallBack(0.2f, std::bind(&ATitleLogo::RendererOn, this));
-
-	// ++Index;
-}
-
 void ATitleLogo::BeginPlay()
 {
 	Super::BeginPlay();
 
 	SetActorScale3D(FVector(700.0f, 300.0f, 100.0f));
 
-	DelayCallBack(1.0f, std::bind(&ATitleLogo::RendererOff, this));
-
-	// Renderer->SetSamplering(ETextureSampling::LINEAR);
-	Renderer->SetSprite("CuttingTest.png", 11);
-
-	Renderer->CreateAnimation("Run", "Run", 1.0f, true, 5);
+	Renderer->CreateAnimation("Run", "Run", 0.1f);
 
 	Renderer->ChangeAnimation("Run");
 
